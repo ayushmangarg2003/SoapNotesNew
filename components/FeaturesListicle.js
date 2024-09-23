9b8d019f6663abc6f29c2c2b9818c95b1d88c3fa
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-09-23 14:08:59 +0530

    Features updated

--- components/FeaturesListicle.js
+++ components/FeaturesListicle.js
@@ -6,16 +6,17 @@ import { useState, useEffect, useRef } from "react";
 // - name: name of the feature
 // - description: description of the feature (can be any JSX)
 // - svg: icon of the feature
+
 const features = [
   {
-    name: "Emails",
+    name: "Scribing",
     description: (
       <>
         <ul className="space-y-1">
           {[
-            "Send transactional emails",
-            "DNS setup to avoid spam folder (DKIM, DMARC, SPF in subdomain)",
-            "Webhook to receive & forward emails",
+            "Real-Time Capturing",
+            "Automated scribing enhances efficiency and accuracy",
+            "Simplifies the process of approving and releasing summaries",
           ].map((item) => (
             <li key={item} className="flex items-center gap-3">
               <svg
@@ -69,14 +70,14 @@ const features = [
     ),
   },
   {
-    name: "Payments",
+    name: "Clearinghouse",
     description: (
       <>
         <ul className="space-y-2">
           {[
-            "Create checkout sessions",
-            "Handle webhooks to update user's account",
-            "Tips to setup your account & reduce chargebacks",
+            "Actively addresses and mitigates claim denials.",
+            "AI streamlines coding and claim submission processes",
+            "Reduces the need for manual back-and-forth communication",
           ].map((item) => (
             <li key={item} className="flex items-center gap-3">
               <svg
@@ -131,15 +132,15 @@ const features = [
     ),
   },
   {
-    name: "Login",
+    name: "Inventory",
     description: (
       <>
         <ul className="space-y-2">
           {[
-            "Magic links setup",
-            "Login with Google walkthrough",
-            "Save user data in MongoDB",
-            "Private/protected pages & API calls",
+            "One-Click Reordering",
+            "System learns usage patterns for each appointment.",
+            "Automatic Reorders",
+            "Simplifies inventory management for efficiency",
           ].map((item) => (
             <li key={item} className="flex items-center gap-3">
               <svg
@@ -194,11 +195,15 @@ const features = [
     ),
   },
   {
-    name: "Database",
+    name: "Customizable",
     description: (
       <>
         <ul className="space-y-2">
-          {["Mongoose schema", "Mongoose plugins to make your life easier"].map(
+          {["Customizable add-ons designed for specialty practices",
+            "Broad range of features to meet specific automation needs",
+            "Easily adapt the system to fit unique practice requirements",
+            "Optimizes workflow for various specialty fields",
+          ].map(
             (item) => (
               <li key={item} className="flex items-center gap-3">
                 <svg
@@ -254,16 +259,15 @@ const features = [
     ),
   },
   {
-    name: "SEO",
+    name: "Follow-up",
     description: (
       <>
         <ul className="space-y-2">
           {[
-            "All meta tags to rank on Google",
-            "OpenGraph tags to share on social media",
-            "Automated sitemap generation to fasten Google indexing",
-            "Structured data markup for Rich Snippets",
-            "SEO-optimized UI components",
+            "Sends reminders to high-risk patients for office visits",
+            "Utilizes AI for effective follow-up management",
+            "Automatically messages patients with care instructions.",
+            "Provides follow-up consultation details seamlessly",
           ].map((item) => (
             <li key={item} className="flex items-center gap-3">
               <svg
@@ -313,68 +317,6 @@ const features = [
           strokeLinecap="round"
           strokeLinejoin="round"
           d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m5.231 13.481L15 17.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v16.5c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9zm3.75 11.625a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z"
-        />
-      </svg>
-    ),
-  },
-  {
-    name: "Style",
-    description: (
-      <>
-        <ul className="space-y-2">
-          {[
-            "Components, animations & sections (like the pricing page below)",
-            "20+ themes with daisyUI",
-            "Automatic dark mode",
-          ].map((item) => (
-            <li key={item} className="flex items-center gap-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="w-[18px] h-[18px] inline shrink-0 opacity-80"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                  clipRule="evenodd"
-                />
-              </svg>
-
-              {item}
-            </li>
-          ))}
-          <li className="flex items-center gap-3 text-accent font-medium">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-              className="w-[18px] h-[18px] inline shrink-0"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                clipRule="evenodd"
-              />
-            </svg>
-            Time saved: 5 hours
-          </li>
-        </ul>
-      </>
-    ),
-    svg: (
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1.5}
-        stroke="currentColor"
-        className="w-8 h-8"
-      >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9.53 16.122a3 3 0 00-5.78 1.128 2.25 2.25 0 01-2.4 2.245 4.5 4.5 0 008.4-2.245c0-.399-.078-.78-.22-1.128zm0 0a15.998 15.998 0 003.388-1.62m-5.043-.025a15.994 15.994 0 011.622-3.395m3.42 3.42a15.995 15.995 0 004.764-4.648l3.876-5.814a1.151 1.151 0 00-1.597-1.597L14.146 6.32a15.996 15.996 0 00-4.649 4.763m3.42 3.42a6.776 6.776 0 00-3.42-3.42"
         />
       </svg>
     ),
@@ -434,17 +376,16 @@ const FeaturesListicle = () => {
       <div className="max-w-3xl mx-auto">
         <div className="bg-base-100 max-md:px-8 max-w-3xl">
           <p className="text-accent font-medium text-sm font-mono mb-3">
-            const launch_time = &quot;Today&quot;;
+            {/* const launch_time = &quot;Today&quot;; */}
+            What we do
           </p>
           <h2 className="font-extrabold text-3xl lg:text-5xl tracking-tight mb-8">
             {/* 💡 COPY TIP: Remind visitors about the value of your product. Why do they need it? */}
-            Supercharge your app instantly, launch faster, make $
+            EHR platform that automates your entire workflow
           </h2>
           <div className="text-base-content/80 leading-relaxed mb-8 lg:text-lg">
             {/* 💡 COPY TIP: Explain how your product delivers what you promise in the headline. */}
-            Login users, process payments and send emails at lightspeed. Spend
-            your time building your startup, not integrating APIs. ShipFast
-            provides you with the boilerplate code you need to launch, FAST.
+            Through ambient listening, our AI-powered scribing solution captures and summarizes your patient encounters in real-time. Dictation-based edits available before approval and release.
           </div>
         </div>
       </div>
@@ -461,20 +402,18 @@ const FeaturesListicle = () => {
               className={`flex flex-col items-center justify-center gap-3 select-none cursor-pointer p-2 duration-200 group`}
             >
               <span
-                className={`duration-100 ${
-                  featureSelected === feature.name
-                    ? "text-primary"
-                    : "text-base-content/30 group-hover:text-base-content/50"
-                }`}
+                className={`duration-100 ${featureSelected === feature.name
+                  ? "text-primary"
+                  : "text-base-content/30 group-hover:text-base-content/50"
+                  }`}
               >
                 {feature.svg}
               </span>
               <span
-                className={`font-semibold text-sm ${
-                  featureSelected === feature.name
-                    ? "text-primary"
-                    : "text-base-content/50"
-                }`}
+                className={`font-semibold text-sm ${featureSelected === feature.name
+                  ? "text-primary"
+                  : "text-base-content/50"
+                  }`}
               >
                 {feature.name}
               </span>
