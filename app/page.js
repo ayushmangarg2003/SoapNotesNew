039f4f44d4dcf506d3028c8ff908b7666ad6158f
Ayushman Garg <105537793+ayushmangarg2003@users.noreply.github.com>
2024-09-24 23:02:21 +0530

    Design changes on home page and login with google implemented

--- app/page.js
+++ app/page.js
@@ -1,5 +1,3 @@
-import Link from "next/link";
-import ButtonSignin from "@/components/ButtonSignin";
 import Hero from "@/components/Hero";
 import Header from "@/components/Header";
 import FeaturesListicle from "@/components/FeaturesListicle";
@@ -13,7 +11,6 @@ export default function Page() {
       <Hero
         heading={'Genesis Automation ✨'}
         description={'Genesis, your intelligent partner in healthcare automation. STOP TYPING, START TREATING...'}
-        img="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
         btnLink={redirectLink}
       />
       <FeaturesListicle />
--- components/Hero.js
+++ components/Hero.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import TestimonialsAvatars from "./TestimonialsAvatars";
 import config from "@/config";
-
+import homeIMG from "../assets/images/homeIMG.jpg"
 const Hero = (props) => {
   return (
     <section className="max-w-7xl mx-auto bg-base-100 flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
@@ -48,7 +48,7 @@ const Hero = (props) => {
       </div>
       <div className="lg:w-full">
         <Image
-          src={props.img}
+          src={homeIMG}
           alt="Product Demo"
           className="w-full"
           priority={true}
